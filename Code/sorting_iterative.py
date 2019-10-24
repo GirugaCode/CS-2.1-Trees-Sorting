31e8f25426a13e8de3ab69bd6e0ef914dc46f57d
Ryan Nguyen <ryan.nguyen@students.makeschool.com>
2019-10-24 12:51:11 -0700

    [add] is_sorted & bubble_sort implemented

--- Code/sorting_iterative.py
+++ Code/sorting_iterative.py
@@ -5,7 +5,20 @@ def is_sorted(items):
     """Return a boolean indicating whether given items are in sorted order.
     TODO: Running time: ??? Why and under what conditions?
     TODO: Memory usage: ??? Why and under what conditions?"""
-    # TODO: Check that all adjacent items are in order, return early if so
+    # [3,6,1,2,12,4] 
+    curr_item = 0
+    for _ in range(1, len(items)):
+        if items[curr_item] > items[curr_item + 1]:
+            return False
+        curr_item += 1
+    return True
+
+    # First Approach 
+        # for _ in items:
+        #     if items[tracker] > items[tracker + 1]:
+        #         return False
+        #     tracker += 1
+        # return True
 
 
 def bubble_sort(items):
@@ -13,8 +26,24 @@ def bubble_sort(items):
     repeating until all items are in sorted order.
     TODO: Running time: ??? Why and under what conditions?
     TODO: Memory usage: ??? Why and under what conditions?"""
-    # TODO: Repeat until all items are in sorted order
-    # TODO: Swap adjacent items that are out of order
+    num_items = len(items) - 1
+    is_sorted = False
+
+    while not is_sorted:
+        is_sorted = True
+        for item in range(num_items):
+            if items[item] > items[item + 1]:
+                is_sorted = False
+                items[item], items[item + 1] = items[item + 1], items[item]
+
+
+    # curr_item = 0
+    # for _ in range(1, len(items)):
+    #     if items[curr_item] < items[curr_item + 1]:
+    #         items[curr_item], items[curr_item + 1] = items[curr_item + 1], items[curr_item]
+    #     curr_item += 1
+    # return items
+
 
 
 def selection_sort(items):
